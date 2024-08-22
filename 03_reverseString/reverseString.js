53c5685721a497b6162591a647d3771e9259d2e2
Jan Reburiano <josephreburiano1212@gmail.com>
2024-08-21 21:52:24 -0700

    Finish javascript assignments 01 02

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let word = "";
+    for (let i = phrase.length - 1; i >= 0; i--) {
+        word += phrase[i];
+    }
+    return word;
 };
 
 // Do not edit below this line
