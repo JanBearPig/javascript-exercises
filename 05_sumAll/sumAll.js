426f72e43d125429e092c065cea976cf293cdd41
Jan Reburiano <josephreburiano1212@gmail.com>
2024-10-01 21:45:32 -0700

    Check for only whole number integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,10 +2,15 @@ const sumAll = function(start, end) {
     let sum = 0;
     let max = Math.max(start, end);
     let min = Math.min(start, end);
-    for (let i = min; i <= max; i++) {
-        sum += i;
+    
+    if ((start * end) > 0 && Number.isInteger(start + end)) {
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return 'ERROR';
     }
-    return sum;
 };
 
 // Do not edit below this line
