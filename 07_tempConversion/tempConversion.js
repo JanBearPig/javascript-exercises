f16feae3f7f0282f78f2a2027a7107cc786b10a8
Jan Reburiano <josephreburiano1212@gmail.com>
2025-01-06 23:26:54 -0800

    temp conversion update

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsiusFinal = ((fahrenheit - 32) * (5/9)).toFixed(1);
+  return Number(celsiusFinal);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheitFinal = ((celsius * (9/5)) + 32).toFixed(1);
+  return Number(fahrenheitFinal);
 };
 
 // Do not edit below this line
